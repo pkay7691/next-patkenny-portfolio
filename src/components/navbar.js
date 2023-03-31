06ef5ecea738ad316411a3e0ac93c46c9e23cdc6
Patrick Kenny <patrickmkenny91@gmail.com>
2023-03-31 17:54:39 -0500

    picture

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar () {
+
+  return(
+    <div className='navbar'>
+      <div className="navbar-button-group">
+      <button>Home</button>
+      <button>About</button>
+      <button>Projects</button>
+      <button>Contact</button>
+      </div>
+    </div>
+  )
+}
